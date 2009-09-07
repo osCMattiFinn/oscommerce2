571fe9c6e332e21df38b6340f3af023cb3d47124
Mark Evans <markevans@mark-evanss-macbook-pro.local>
2009-09-07 19:50:05 +0100

    Revert change for OSC-1005

--- catalog/includes/application_top.php
+++ catalog/includes/application_top.php
@@ -73,7 +73,7 @@
   }
 
 // if gzip_compression is enabled, start to buffer the output
-  if ( (GZIP_COMPRESSION == 'true') && ($ext_zlib_loaded == extension_loaded('zlib')) && (PHP_VERSION >= '4') ) {
+  if ( (GZIP_COMPRESSION == 'true') && ($ext_zlib_loaded = extension_loaded('zlib')) && (PHP_VERSION >= '4') ) {
     if (($ini_zlib_output_compression = (int)ini_get('zlib.output_compression')) < 1) {
       if (PHP_VERSION >= '4.0.4') {
         ob_start('ob_gzhandler');
